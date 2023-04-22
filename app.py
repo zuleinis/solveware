57e5a2024cb5bbb4d28d3124215b9cafc86eba96
Bryan-otero <Bryanoteropsn@gmail.com>
2023-04-22 17:18:51 -0400

    Connected app to Atlas(MongoDB)

--- app.py
+++ app.py
@@ -1,21 +1,41 @@
 #this is Bryan's branch
-# Define tus rutas de Flask
+from flask import Flask, request, render_template
+import db
 
-
-from flask import Flask, render_template
-from models import InterfazUsuario
+# from models import InterfazUsuario
 # from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# Crear una conexión a la base de datos
-# client = MongoClient("<MongoDB URI>")
-# db = client.databaseName
-# collection = db.collectionName
-
-@app.route('/')
+@app.route('/', methods =["GET", "POST"])
 def home():
+    if request.method == "POST":
+        # getting input with name = fname in HTML form
+       nombre = request.form.get("nombre")
+       
+       db.db.test1.insert_one({"nombre" : nombre})
+       
     return render_template('formulario.html')
+
+# def test():
+    
+#     modelo_reporte = {
+#     "numero_reporte": None,
+#     "estado": "abierto",
+#     "datos": {
+#         "usuario": None,
+#         "descripcion": None,
+#         "tecnico": None
+#     }
+# }
+    
+#     # Create collection
+#     db.db.test.insert_one(modelo_reporte)
+    
+#     # find rows in collection that meets condition
+#     for user in db.db.test.find({"estado": "abierto"}):
+#         print(user)
+#     return "Connected to the data base!"
 
 if __name__ == '__main__':
     app.run(debug=True)
